2db9edd642932613b6ddbc4038f75f7fa878d6e6
Chris Anders <filmee24@gmail.com>
2020-12-01 15:53:16 +0100

    Started imple,enting custom type system

---
+++ BookGenerator.Core/RuntimeLibrary/RuntimeStruct.cs
@@ -0,0 +1,14 @@
+﻿using Schemy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace BookGenerator.Core.RuntimeLibrary
+{
+    public class RuntimeStruct : List<object>
+    {
+        public Symbol Typename { get; set; }
+        public Symbol[] PropertyNames { get; set; }
+    }
+}
--- BookGenerator.Core/RuntimeLibrary/SchemeCliLoader.cs
+++ BookGenerator.Core/RuntimeLibrary/SchemeCliLoader.cs
@@ -10,6 +10,39 @@ namespace BookGenerator.Core.RuntimeLibrary
     {
         public static void Apply(Assembly ass, Interpreter interpreter)
         {
+            //(make-struct 'name (list 'prop1 'prop2))
+            interpreter.DefineGlobal(Symbol.FromString("make-struct"), new NativeProcedure(_ =>
+            {
+                var s = new RuntimeStruct { Typename = (Symbol)_.First() };
+
+                var props = (List<object>)_.Last();
+
+                for (int i = 0; i < props.Count - 1; i++)
+                {
+                    s.Add(null);
+                    var prop = (Symbol)props[i];
+
+                    //define getter
+                    interpreter.DefineGlobal(Symbol.FromString("get-" + prop.AsString), new NativeProcedure(_ =>
+                    {
+                        return ((RuntimeStruct)_.First())[i];
+                    }));
+
+                    //define setter
+                    interpreter.DefineGlobal(Symbol.FromString("set-" + prop.AsString + "!"), new NativeProcedure(_ =>
+                    {
+                        ((RuntimeStruct)_.First())[i] = _.Last();
+
+                        return true;
+                    }));
+                }
+
+                //define predicate
+                //define ctor
+
+                return s;
+            }));
+
             foreach (var t in ass.GetTypes())
             {
                 var att = t.GetCustomAttribute<RuntimeTypeAttribute>();
