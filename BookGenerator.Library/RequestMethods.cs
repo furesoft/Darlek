75dc85b7f97a2f368a23f313b36ca748a74e3c5f
Chris Anders <filmee24@gmail.com>
2020-12-01 17:54:33 +0100

    Add HttpRequest methods

---
+++ BookGenerator.Library/RequestMethods.cs
@@ -0,0 +1,51 @@
+﻿using BookGenerator.Core.API;
+using BookGenerator.Core.RuntimeLibrary;
+using Furesoft.Core;
+using Schemy;
+using System.Collections.Generic;
+
+namespace BookGenerator.Library
+{
+    [RuntimeType]
+    public static class RequestMethods
+    {
+        [RuntimeMethod("make-request")]
+        public static object Make()
+        {
+            return new XmlHttpRequest();
+        }
+
+        [RuntimeMethod("request-open")]
+        public static object Open(XmlHttpRequest req, string method, string url)
+        {
+            req.Open(method, url);
+
+            return None.Instance;
+        }
+
+        [RuntimeMethod("request-send")]
+        public static object Send(XmlHttpRequest req, object data)
+        {
+            req.Send(data);
+
+            return None.Instance;
+        }
+
+        [RuntimeMethod("request-subscribe-readystatechanged")]
+        public static object OnChange(XmlHttpRequest req, Procedure callback)
+        {
+            req.OnReadyStateChange += () =>
+            {
+                callback.Call(new List<object> { req.ReadyState });
+            };
+
+            return None.Instance;
+        }
+
+        [RuntimeMethod("request-get-response")]
+        public static object GetResponse(XmlHttpRequest req)
+        {
+            return req.ResponseText;
+        }
+    }
+}
