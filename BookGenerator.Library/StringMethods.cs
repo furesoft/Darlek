5722249d0dbb37ad546d49149379c6626a6aa0ce
Chris Anders <filmee24@gmail.com>
2020-12-02 17:31:36 +0100

    Cleanup

--- BookGenerator.Library/StringMethods.cs
+++ BookGenerator.Library/StringMethods.cs
@@ -16,18 +16,4 @@ namespace BookGenerator.Library
             return src.Substring(start, length);
         }
     }
-
-    [RuntimeType]
-    [RuntimeCtorMethod("point")]
-    public struct Point
-    {
-        public Point(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        public int X { get; set; }
-        public int Y { get; set; }
-    }
 }
