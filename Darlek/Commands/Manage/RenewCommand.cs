7157aef8680f7a7152722b2796dcdb55ae50a5fc
filmee24 <filmee24@gmail.com>
2022-09-04 17:51:23 +0200

    Add RenewCommand

---
+++ Darlek/Commands/Manage/RenewCommand.cs
@@ -0,0 +1,32 @@
+﻿using Darlek.Core;
+using LiteDB;
+using System;
+using System.IO;
+
+namespace Darlek.Commands.Manage;
+
+public class RenewCommand : IMenuCommand
+{
+    public RenewCommand(BsonDocument selectedRecipe)
+    {
+        SelectedRecipe = selectedRecipe;
+    }
+
+    public BsonDocument SelectedRecipe { get; set; }
+
+    public void Invoke(Menu parentMenu)
+    {
+        var crawler = CrawlerFactory.GetCrawler(Repository.GetMetadata("crawler") ?? "chefkoch");
+        var url = SelectedRecipe["url"];
+
+        var r = crawler.Crawl(new Uri(url, UriKind.RelativeOrAbsolute)).Result;
+        r["_id"] = SelectedRecipe["_id"];
+
+        Repository.Update(r);
+
+        if (r.ContainsKey("imageUri"))
+        {
+            Repository.AddFile(r["imageUri"], r["_id"].RawValue.ToString(), r["_id"].RawValue + Path.GetExtension(r["imageUri"]));
+        }
+    }
+}
