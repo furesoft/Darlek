811dfa0ee45c11c60664a5e48abfd6a0ee92b3d6
filmee24 <filmee24@gmail.com>
2022-09-13 06:30:05 +0200

    Add html to scheme

---
+++ Darlek.Library/HtmlMethods.cs
@@ -0,0 +1,29 @@
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using Darlek.Core.RuntimeLibrary;
+
+namespace Darlek.Library;
+
+[RuntimeType("html")]
+public static class HtmlMethods
+{
+    [RuntimeMethod("parse-document")]
+    public static IHtmlDocument Parse(string content)
+    {
+        var parser = new HtmlParser();
+        return parser.ParseDocument(content);
+    }
+
+    [RuntimeMethod("query-selector-all")]
+    public static IHtmlCollection<IElement> QuerySelectorAll(IHtmlDocument document, string selector)
+    {
+        return document.QuerySelectorAll(selector);
+    }
+
+    [RuntimeMethod("query-selector")]
+    public static IElement QuerySelector(IHtmlDocument document, string selector)
+    {
+        return document.QuerySelector(selector);
+    }
+}
