7f7f11ddad07cf811877a286d0e50e8d765fd340
Chris <filmee24@gmail.com>
2022-10-10 17:28:25 +0200

    Add Stack Methods

---
+++ Darlek.Library/StackMethods.cs
@@ -0,0 +1,53 @@
+﻿using Darlek.Core.RuntimeLibrary;
+using Darlek.Core.Schemy;
+using System.Collections.Generic;
+
+namespace Darlek.Library;
+
+[RuntimeType]
+public static class StackMethods
+{
+    [RuntimeMethod("make-stack")]
+    public static Stack<object> Make()
+    {
+        return new Stack<object>();
+    }
+
+    [RuntimeMethod("stack-push!")]
+    public static object Push(Stack<object> stack, object value)
+    {
+        stack.Push(value);
+
+        return None.Instance;
+    }
+
+    [RuntimeMethod("stack-pop!")]
+    public static object Pop(Stack<object> stack)
+    {
+        return stack.Pop();
+    }
+
+    [RuntimeMethod("stack?")]
+    public static bool Pred(object stack)
+    {
+        return stack is Stack<object>;
+    }
+
+    [RuntimeMethod("stack-empty?")]
+    public static bool IsEmpty(Stack<object> stack)
+    {
+        return stack.Count == 0;
+    }
+
+    [RuntimeMethod("stack-length")]
+    public static int Length(Stack<object> stack)
+    {
+        return stack.Count;
+    }
+
+    [RuntimeMethod("stack-top")]
+    public static object Top(Stack<object> stack)
+    {
+        return stack.Peek();
+    }
+}
